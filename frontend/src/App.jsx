6d5bfcca75c5bfb96b401337592bb56c5d3c62f4
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2026-04-26 22:28:41 +0300

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400 underline decoration-sky-600">
+        Vite + Tailwind v4 is live!
+      </h1>
+    </div>
+  )
+}
