03a419fd01f2d62f66274d3a0289191a3effb338
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2026-04-28 00:26:36 +0300

    configure supabase and Authcontext file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-function App() {
+export default function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-sky-400 underline decoration-sky-600">
